1dbdd0b038a4d07d4cc63b7b9ddd8a064737cc3a
qiuquanhui <qiuquanhui@users.noreply.github.com>
2026-09-15 19:44:18 +0800

    增加ollama的调用方式

--- src/main/java/com/hui/huiaiagent/demo/invoke/OllamaAiInvoke.java
+++ src/main/java/com/hui/huiaiagent/demo/invoke/OllamaAiInvoke.java
@@ -1,24 +1,28 @@
 package com.hui.huiaiagent.demo.invoke;
 
 import jakarta.annotation.Resource;
+
 import org.springframework.ai.chat.messages.AssistantMessage;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-// 取消注释即可在 SpringBoot 项目启动时执行
+/**
+ * Spring AI 框架调用 AI 大模型（Ollama）
+ */
+// 取消注释后，项目启动时会执行
 //@Component
-public class SpringAiAiInvoke implements CommandLineRunner {
+public class OllamaAiInvoke implements CommandLineRunner {
 
     @Resource
-    private ChatModel dashscopeChatModel;
+    private ChatModel ollamaChatModel;
 
     @Override
     public void run(String... args) throws Exception {
-        AssistantMessage output = dashscopeChatModel.call(new Prompt("你好，我是小辉"))
+        AssistantMessage assistantMessage = ollamaChatModel.call(new Prompt("你好，我是小辉"))
                 .getResult()
                 .getOutput();
-        System.out.println(output.getText());
+        System.out.println(assistantMessage.getText());
     }
 }
